1bbfd1c1a8287915cdd973c6411f74df3910be09
Jake Standley <jakestandley95@gmail.com>
2021-09-16 17:22:18 -0500

    Update driver script
    Changed driver script to now only test the desired outcome: the knight_moves method

--- main.rb
+++ main.rb
@@ -3,18 +3,8 @@
 require_relative 'data_structure'
 require_relative 'cell'
 
-root_cell = KnightsTravails::Cell.new([3, 3])
-# test_cell = KnightsTravails::Cell.new([1, 4])
+root_cell = KnightsTravails::Cell.new([7, 0])
+target_cell = KnightsTravails::Cell.new([0, 0])
 my_data_structure = KnightsTravails::DataStructure.new(root_cell)
 
-puts 'Full structure'
-p my_data_structure
-
-# p my_data_structure.level_order.count
-# p my_data_structure.level_order.uniq.count
-
-p my_data_structure.level_order
-
-p my_data_structure.level_order.count
-
-# p my_data_structure.all_possible_destination_cells(test_cell)
+my_data_structure.knight_moves(target_cell)
